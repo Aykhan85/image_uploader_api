b1636a0cd9ae427977b116673a89c7079b9bcb9f
OMEN <ayxannematov96@gmail.com>
2023-01-17 16:26:09 +0400

    backend is ready to use

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://image-uplader.onrender.com"];
+const allowedOrigins = [
+  "https://image-uplader.onrender.com",
+  "https://image-uploader-api-4ctd.onrender.com",
+];
 
 module.exports = allowedOrigins;
